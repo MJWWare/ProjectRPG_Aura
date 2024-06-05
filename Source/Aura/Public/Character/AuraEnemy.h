66e7042f2c09d4170e62911efa68d174507de594
Mark J Williams <mjwdroid@gmail.com>
2024-06-05 16:33:02 -0400

    section 2 chap 14 done

--- Source/Aura/Public/Character/AuraEnemy.h
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -16,9 +16,11 @@ class AURA_API AAuraEnemy : public AAuraCharacterBase, public IEnemyInterface
 	GENERATED_BODY()
 
 public:
+	AAuraEnemy();
 	virtual void HighlightActor() override;
 	virtual void UnhighlightActor() override;
 
-	UPROPERTY(BlueprintReadOnly)
-	bool bHighlighted = false;
+	//UPROPERTY(BlueprintReadOnly)
+	//bool bHighlighted = false;
+	
 };
