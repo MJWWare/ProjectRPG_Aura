a6a91e52579098045fc187cf3e6545ea6fedca35
Mark J Williams <mjwdroid@gmail.com>
2024-06-04 22:28:29 -0400

    section 2 chap 13 done

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -5,11 +5,19 @@
 
 #include "EnhancedInputSubsystems.h"
 #include "EnhancedInputComponent.h"
+#include "Interaction/EnemyInterface.h"
 
 AAuraPlayerController::AAuraPlayerController()
 {
 	bReplicates = true;
 	
+}
+
+void AAuraPlayerController::PlayerTick(float DeltaTime)
+{
+	Super::PlayerTick(DeltaTime);
+
+	CursorTrace();
 }
 
 void AAuraPlayerController::BeginPlay()
@@ -54,5 +62,52 @@ void AAuraPlayerController::Move(const FInputActionValue& InputActionValue)
 		ControlledPawn->AddMovementInput(ForwardDirection, InputAxisVector.Y);
 		ControlledPawn->AddMovementInput(RightDirection, InputAxisVector.X);
 		
+	}
+}
+
+void AAuraPlayerController::CursorTrace()
+{
+	FHitResult CursorHit;
+	GetHitResultUnderCursor(ECC_Visibility, false, CursorHit);
+	if (!CursorHit.bBlockingHit) return;
+
+	/* LastActor = ThisActor;
+	ThisActor = Cast<IEnemyInterface>(CursorHit.GetActor()); */
+
+	LastActor = ThisActor;
+	ThisActor = CursorHit.GetActor();
+
+	if (LastActor == nullptr)
+	{
+		if (ThisActor != nullptr)
+		{
+			//case b
+			ThisActor->HighlightActor();
+		}
+		else
+		{
+			//case a nothing
+		}
+	}
+	else //last is valid
+	{
+		if (ThisActor == nullptr)
+		{
+			//case c
+			LastActor->UnhighlightActor();
+		}
+		else //both valid
+		{
+			if (LastActor != ThisActor)
+			{
+				//case d
+				LastActor->UnhighlightActor();
+				ThisActor->HighlightActor();
+			}
+			else
+			{
+				//case e nothing
+			}
+		}
 	}
 }
