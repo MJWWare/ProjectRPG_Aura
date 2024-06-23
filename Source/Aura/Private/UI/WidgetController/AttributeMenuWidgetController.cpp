f6f04f8d775025b575d31992d6d80b8b062b68e9
Mark J Williams <mjwdroid@gmail.com>
2024-06-23 17:03:16 -0400

    section 9 chap 89 done

---
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,14 @@
+// Copyright MJWWare
+
+
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+	
+}
+
+void UAttributeMenuWidgetController::BroadcastInitialValues()
+{
+	
+}
