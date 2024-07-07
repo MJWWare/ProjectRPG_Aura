c75e090071714f4641eda7cb29d94bdd0c5a4abe
Mark J Williams <mjwdroid@gmail.com>
2024-07-07 14:40:24 -0400

    Aura chap 151 done

---
+++ Source/Aura/Public/AuraAbilityTypes.h
@@ -0,0 +1,37 @@
+﻿// Copyright MJWWare
+
+#pragma once
+
+#include "GameplayEffectTypes.h"
+#include "AuraAbilityTypes.generated.h"
+
+USTRUCT(BlueprintType)
+struct FAuraGameplayEffectContext : public FGameplayEffectContext
+{
+	GENERATED_BODY()
+
+public:
+
+	bool IsCritHit() const { return bIsCritHit; }
+	bool IsBlockedHit() const { return bIsBlockedHit; }
+
+	void SetIsCritHit(bool bInIsCritHit) { bIsCritHit = bInIsCritHit; }
+	void SetIsBlockedHit(bool bInIsBlockedHit) { bIsBlockedHit = bInIsBlockedHit; }
+	
+	/** Returns the actual struct used for serialization, subclasses must override this! */
+	virtual UScriptStruct* GetScriptStruct() const
+	{
+		return FGameplayEffectContext::StaticStruct();
+	}
+
+	/** Custom serialization, subclasses must override this */
+	virtual bool NetSerialize(FArchive& Ar, class UPackageMap* Map, bool& bOutSuccess);
+	
+protected:
+
+	UPROPERTY()
+	bool bIsBlockedHit = false;
+	
+	UPROPERTY()
+	bool bIsCritHit = false;
+};
