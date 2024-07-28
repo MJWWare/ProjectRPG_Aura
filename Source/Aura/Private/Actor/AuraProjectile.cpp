9e03cde305f4b02ad8f276d446a30278ee0549db
Mark J Williams <mjwdroid@gmail.com>
2024-07-27 20:25:09 -0400

    Aura chap 229 done

--- Source/Aura/Private/Actor/AuraProjectile.cpp
+++ Source/Aura/Private/Actor/AuraProjectile.cpp
@@ -68,6 +68,7 @@ void AAuraProjectile::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, 
 	}
 	if(!bHit)
 	{
+		//UE_LOG(LogTemp, Warning, TEXT("[%s] projectiles spawned"), *GetName())
 		UGameplayStatics::PlaySoundAtLocation(this, ImpactSound, GetActorLocation(), FRotator::ZeroRotator);
 		UNiagaraFunctionLibrary::SpawnSystemAtLocation(this, ImpactEffect, GetActorLocation());
 		if(LoopingSoundComponent) LoopingSoundComponent->Stop();
