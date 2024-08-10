ae2be5f4d2eb300a2fe6d64d57a198ed228906d9
Mark J Williams <mjwdroid@gmail.com>
2024-08-10 15:43:52 -0400

    done through Aura chap 262

--- Source/Aura/Public/Character/AuraCharacter.h
+++ Source/Aura/Public/Character/AuraCharacter.h
@@ -7,6 +7,9 @@
 #include "Interaction/PlayerInterface.h"
 #include "AuraCharacter.generated.h"
 
+class UCameraComponent;
+class USpringArmComponent;
+class UNiagaraComponent;
 /**
  * 
  */
@@ -33,8 +36,18 @@ public:
 	/** Combat Interface **/
 	virtual int32 GetPlayerLevel_Implementation() override;
 
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TObjectPtr<UNiagaraComponent> LevelUpNiagaraComponent;
 	
 private:
-	virtual  void InitAbilityActorInfo() override;
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UCameraComponent> TopDownCameraComponent;
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<USpringArmComponent> CameraBoom;
 	
+	virtual  void InitAbilityActorInfo() override;
+
+	UFUNCTION(NetMulticast, Reliable)
+	void MulticastLevelUpParticles() const;
 };
