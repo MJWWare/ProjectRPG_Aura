8df6a1c1f0c2604196d57c8651595f3091464528
Mark J Williams <mjwdroid@gmail.com>
2024-08-13 22:57:51 -0400

    done through Aura chap 278

---
+++ Source/Aura/Private/UI/WidgetController/SpellMenuWidgetController.cpp
@@ -0,0 +1,14 @@
+// Copyright MJWWare
+
+
+#include "UI/WidgetController/SpellMenuWidgetController.h"
+
+void USpellMenuWidgetController::BroadcastInitialValues()
+{
+	
+}
+
+void USpellMenuWidgetController::BindCallbacksToDependencies()
+{
+	
+}
