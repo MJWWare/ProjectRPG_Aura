14505d6bd310133520e4e67cd1c2c6674ea972fa
Mark J Williams <mjwdroid@gmail.com>
2024-09-08 14:27:35 -0400

    done through Aura chap 345

---
+++ Source/Aura/Private/Actor/MagicCircle.cpp
@@ -0,0 +1,27 @@
+// Copyright MJWWare
+
+
+#include "Actor/MagicCircle.h"
+#include "Components/DecalComponent.h"
+
+// Sets default values
+AMagicCircle::AMagicCircle()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	MagicCircleDecal = CreateDefaultSubobject<UDecalComponent>("MagicCircleDecal");
+	MagicCircleDecal->SetupAttachment(GetRootComponent());
+}
+
+void AMagicCircle::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AMagicCircle::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/Aura/Public/Actor/MagicCircle.h
@@ -0,0 +1,25 @@
+// Copyright MJWWare
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MagicCircle.generated.h"
+
+UCLASS()
+class AURA_API AMagicCircle : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AMagicCircle();
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UDecalComponent> MagicCircleDecal;
+};
